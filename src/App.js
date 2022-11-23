3fd81d6092dc20e791ebedf6bb06f49573b47334
MaxVdp <100362070+MaxVdp@users.noreply.github.com>
2022-11-23 11:36:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import CollectionForm from './Form'
 
 class App extends Component{
 
-    API_URL="http://193.191.176.89:9000"
+    API_URL="http://193.191.176.89:7000"
 
     state = {
       products: [],
